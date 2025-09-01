5e8284e8343551051d8e32fe7bce995f0e17395c
junweiq <aooaqoob@gmail.com>
2025-09-01 21:14:01 +0800

    ping, hello handle 拆分

---
+++ internal/service/hello_handle.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HelloReq struct {
+	Name string `json:"name" binding:"required"`
+}
+
+type HelloRes struct {
+	Message string `json:"message" binding:"required"`
+}
+
+func (c *CmsApp) Hello(ctx *gin.Context) {
+	var req HelloReq
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  "ok",
+		"data": &HelloRes{
+			Message: fmt.Sprintf("hello %s", req.Name),
+		},
+	})
+}
--- internal/service/ping_handle.go
+++ internal/service/ping_handle.go
@@ -1,39 +1,13 @@
 package service
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type HelloReq struct {
-	Name string `json:"name" binding:"required"`
-}
-
-type HelloRes struct {
-	Message string `json:"message" binding:"required"`
-}
-
 func (c *CmsApp) Ping(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "pong",
-	})
-}
-
-func (c *CmsApp) Hello(ctx *gin.Context) {
-	var req HelloReq
-
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": 0,
-		"msg":  "ok",
-		"data": &HelloRes{
-			Message: fmt.Sprintf("hello %s", req.Name),
-		},
 	})
 }

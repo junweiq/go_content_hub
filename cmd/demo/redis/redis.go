85bc9c7dfac7d3fc2e0867ebbbea67f6df8a351e
junweiq <aooaqoob@gmail.com>
2025-09-06 22:33:58 +0800

    新增 redis demo

---
+++ cmd/demo/redis/redis.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+func main() {
+	rdb := connRdb()
+
+	err := rdb.Set(ctx, "SID:admin", "test", 5*time.Second).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	SID, err := rdb.Get(ctx, "SID:admin").Result()
+	if err != nil && !errors.Is(err, redis.Nil) {
+		panic(err)
+	}
+
+	fmt.Println(SID)
+}
+
+func connRdb() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return rdb
+}

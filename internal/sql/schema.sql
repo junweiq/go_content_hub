502544ca1dd6739b9881c0c0a3e07b7c680b9575
junweiq <aooaqoob@gmail.com>
2025-09-10 22:58:48 +0800

    sqlc 改寫中

---
+++ internal/sql/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `t_user_detail`
+(
+    `id`         int       NOT NULL AUTO_INCREMENT COMMENT '主鍵ID',
+    `username`   varchar(64)        DEFAULT '' COMMENT '用戶名',
+    `password`   varchar(64)        DEFAULT '' COMMENT '密碼',
+    `nickname`   varchar(64)        DEFAULT '' COMMENT '暱稱',
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '創建時間',
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='cms用戶訊息';
